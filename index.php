fa98086873f73d8458e46186018268b57addab29
SRUTHYYY <78959156+SRUTHYYY@users.noreply.github.com>
2021-02-15 21:05:50 +0530

    employee

---
+++ index.php
@@ -0,0 +1,46 @@
+<HTML>
+    <HEAD>
+        <titLE>MY WEB</titLE>
+
+</HEAD>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<BODY>
+    <table class="table">
+        <TR>
+            <TD>EMPLOY NAME</TD>
+            <TD><input type="text"></TD>
+        </TR>
+        <TR>
+            <TD>DESIGNATION</TD>
+            <TD><input type="text"></TD>
+        </TR>
+        <TR>
+            <TD>SALARY</TD>
+            <TD><input type="text"></TD>
+        </TR>
+        <TR>
+            <TD>COMPANY NAME</TD>
+            <TD><input type="text"></TD>
+        </TR>
+        <TR>
+            <TD>ADDRESS</TD>
+            <TD><input type="text"></TD>
+        </TR>
+        <tr>
+            <td>DISTRICT</td>
+            <td><input type="text"></td>
+        </tr>
+        <tr>
+            <td>PINCODE</td>
+            <td><input type="text"></td>
+        </tr>
+        <tr>
+            <td>MOBILE NO.</td>
+            <td><input type="text"></td>
+        </tr>
+        <tr>
+        <td><button class="btn btn-secondary"><b>SUBMIT</b></button></td>
+    </tr>
+    </TABLE>
+</BODY>
+</HTML>
